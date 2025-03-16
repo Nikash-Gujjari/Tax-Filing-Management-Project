cd7f37e127fd32cd43e15b40f5a34b28b73ae101
Nikash Gujjari <nikashgujjari@gmail.com>
2025-03-15 22:18:44 -0400

    Configured and Created Maven Project

---
+++ tax-management-system-api/src/main/java/com/skillstorm/tax_management_system_api/TaxManagementSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.skillstorm.tax_management_system_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxManagementSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxManagementSystemApiApplication.class, args);
+	}
+
+}
